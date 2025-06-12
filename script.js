d9d8ca999d0044823e4a1ca54831f5ff027aafa1
Your Name <94960890+Misterpeppa@users.noreply.github.com>
2025-06-12 22:05:28 +0800

    Added different grid sizes

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const container = document.querySelector("#container");
+const btn4 = document.querySelector("#btn4");
+const btn8 = document.querySelector("#btn8");
+const btn16 = document.querySelector("#btn16");
+const btn32 = document.querySelector("#btn32");
 function createGrid() {
     for (let i = 0; i < 16; i++) {
     const row = document.createElement("div");
@@ -13,21 +17,18 @@ function createGrid() {
     
 }
 function removeGrid() {
-    const rows = document.querySelector(".row");
+    const rows = document.querySelectorAll(".row");
     rows.forEach((row) => {
-        container.removeChild(rows)
+        row.remove()
         const column = row.querySelectorAll(".column");
-        container.removeChild(rows);
         column.forEach((col) => {
-            container.removeChild(column);
+            col.remove();
         })
     })
       
 }
 
-document.addEventListener("DOMContentLoaded", (event) => {
-    createGrid();
-
+function etchGrid() {
     const rows = document.querySelectorAll(".row")
     rows.forEach((row) => {
         const column = row.querySelectorAll(".column");
@@ -38,71 +39,69 @@ document.addEventListener("DOMContentLoaded", (event) => {
         });
         });
     })
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    createGrid();
+    etchGrid();
+    btn4.addEventListener("click", function(){
+        removeGrid();
+        for (let i = 0; i < 16; i++) {
+            const row = document.createElement("div");
+            row.classList.add("row");
+            container.appendChild(row);
+            for (let j = 0; j < 16; j++) {
+                const column = document.createElement("div");
+                column.classList.add("column");
+                row.appendChild(column);
+            }
+        }
+    etchGrid();
+    })
+    btn8.addEventListener("click", function() {
+        removeGrid();
+        for (let i = 0; i < 32; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        for(let j = 0; j < 32; j++) {
+            const column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);
+            }
+        }
+    etchGrid();
+    });
+    btn16.addEventListener("click", function() {
+        removeGrid();
+        for (let i = 0; i < 64; i++) {
+            const row = document.createElement("div");
+            row.classList.add("row");
+            container.appendChild(row);
+            for (let j = 0; j < 32; j++) {
+                const column = document.createElement("div");
+                column.classList.add("column");
+                row.appendChild(column);
+            }
+        }
+        etchGrid();
+    })
+    btn32.addEventListener("click", function() {
+        removeGrid();
+        for (let i = 0; i < 128; i++) {
+            const row = document.createElement("div");
+            row.classList.add("row");
+            container.appendChild(row);
+            for (let j = 0; j < 128; j++) {
+                const column = document.createElement("div");
+                column.classList.add("column");
+                row.appendChild(column);
+            }
+        }
+        etchGrid();
+    })
+   
 })
-
-
-// const btn4 = document.querySelector("#btn4");
-const btn8 = document.querySelector("#btn8");
-// const btn16 = document.querySelector("#btn16");
-// const btn32 = document.querySelector("#btn32");
-
-function firstButton() {
-    for (let i = 0; i < 32; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    for(let j = 0; j < 32; j++) {
-        const column = document.createElement("div");
-        column.classList.add("column");
-        row.appendChild(column);
-    }
-    }
-}
-
-
-btn8.addEventListener("click", function() {
-    removeGrid();
-    // for (let i = 0; i < 32; i++) {
-    // const row = document.createElement("div");
-    // row.classList.add("row");
-    // container.appendChild(row);
-    // for(let j = 0; j < 32; j++) {
-    //     const column = document.createElement("div");
-    //     column.classList.add("column");
-    //     row.appendChild(column);
-    // }
-    // }
-});
-
-btn16.addEventListener("click", function() {
-    for (let i = 0; i < 3; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    for(let j = 0; j < 4; j++) {
-        const column = document.createElement("div");
-        column.classList.add("column");
-        row.appendChild(column);
-    }
-}
-});
-
-btn32.addEventListener("click", function() {
-    for (let i = 0; i < 7; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    for(let j = 0; j < 8; j++) {
-        const column = document.createElement("div");
-        column.classList.add("column");
-        row.appendChild(column);
-    }
-}
-});
-
-function clearGrid() {
-    document.getElementById(container).innerHTML = " ";
-}
 
 const clearBtn = document.getElementById("clearBtn");
 
